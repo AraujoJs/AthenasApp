c47c801730a41e922bf52b708e32b231d4b0c6ef
Arthur Lima <mozart.bachh@gmail.com>
2024-02-08 23:37:31 +0100

    Initial commit: Added text strings, images, and wireframe for the login screen. Started the project.

---
+++ app/src/main/java/com/example/athenas/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.athenas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.athenas.ui.theme.AthenasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AthenasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color =  MaterialTheme.colorScheme.background
+
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+
+
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AthenasTheme {
+        Greeting("Android")
+    }
+}
